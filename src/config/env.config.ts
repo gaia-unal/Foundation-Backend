875b57302f71cfabae696b437c1dd2d87f6a9871
JohnPosada <johnposada2000@gmail.com>
2023-04-14 10:46:16 -0500

    set up enviroment variables

---
+++ src/config/env.config.ts
@@ -0,0 +1,11 @@
+export const EnvConfiguration = () => ({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  PORT: process.env.PORT,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+});
