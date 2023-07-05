4b4dfe392574999d1fa22dd8ae875444c5843d93
Julián Pachón Castrillón <jupachon@unal.edu.co>
2023-07-05 15:40:24 -0500

    Added environment variable to fix the postgres default port

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { FoundationModule } from './foundation/foundation.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      port: +process.env.INTERNAL_CONTAINER_DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
